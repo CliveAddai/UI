15fda5eb4de9e17ad8a69cf84b4ea826adf2541c
CliveAddai <44880447+CliveAddai@users.noreply.github.com>
2019-11-05 11:06:34 -0500

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void AudioPlayer()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Pong()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Snake()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
